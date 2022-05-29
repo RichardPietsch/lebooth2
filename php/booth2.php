a74ae1e0fa886e74dd7fb5ed4b1ea94ff3a5fe7b
Richard Pietsch <richard.pietsch@argonauten.de>
2022-05-29 15:07:43 +0200

    added old php shit

--- php/booth2.php
+++ php/booth2.php
@@ -1,16 +1,28 @@
 <?php
-
-if(isset($_POST['action']) && !empty($_POST['action'])) {
-    $action = $_POST['action'];
-    switch($action) {
-        case 'takePicture' : takePicture();break;
-
-    }
+	if(isset($_POST['action']) && !empty($_POST['action'])) {
+		$action = $_POST['action'];
+		switch($action) {
+			case 'bootup' : bootup();break;
+			case 'takepicture' : takepicture();break;
+			case 'updatecounter' : updatecounter();break;
+			default:break;
+		}
 }
 
-function takePicture() {
+	function bootup(){
+	$startnumber = file_get_contents('imagecounter.txt');
+	echo ($startnumber);
+	}
 
-    echo '0000-test';
-}
+	function takepicture(){
+		//exec ('sudo gphoto2 --capture-image-and-download --filename "../images/capture-%Y%m%d-%H%M%S.%C" 2>&1',$out);
+		exec ('sudo gphoto2 --capture-image-and-download --filename "../images/'.$_POST['imagecounter'].'.%C"',$out);
+		echo($_POST['imagecounter']);
+	}
+
+	function updatecounter(){
+		file_put_contents('imagecounter.txt',$_POST['newnumber'],LOCK_EX);
+		echo(file_get_contents('imagecounter.txt'));
+	}
 
 ?>
